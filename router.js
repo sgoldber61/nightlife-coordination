c85411dc2765620845955864b5242a0aab9ee792
Sam Goldberg <sgoldber61@gmail.com>
2018-03-08 19:01:36 -0800

    Initial version of back end

--- router.js
+++ router.js
@@ -1,18 +1,31 @@
-const Authentication = require('./controllers/authentication');
+const AuthenticationOperations = require('./controllers/authentication');
+const APIOperations = require('./controllers/operations');
+
 const passportService = require('./services/passport');
 const passport = require('passport');
 
 const requireAuth = passport.authenticate('jwt', {session: false});
 const requireSignin = passport.authenticate('local', {session: false});
+const optionalAuth = function(req, res, next) {
+  passport.authenticate('jwt', {session: false}, function(err, user, info) {
+    if (err)
+      return next(err);
+    
+    req.user = user; // user data gets sent. lack of authentication means user is set to false
+    next();
+  })(req, res, next);
+}
 
 
 module.exports = function(app) {
-  app.get('/', requireAuth, function(req, res) {
-    res.send({message: "Super secret code is ABC123"});
-  });
+  // authentication operations
+  app.post('/signin', requireSignin, AuthenticationOperations.signin);
+  app.post('/signup', AuthenticationOperations.signup);
   
-  app.post('/signin', requireSignin, Authentication.signin);
-  
-  app.post('/signup', Authentication.signup);
+  // logical operations
+  app.post('/add-bar', requireAuth, APIOperations.addBar); // indicate that we are going to a bar
+  app.post('/remove-bar', requireAuth, APIOperations.removeBar); // indicate that we are no longer going to a bar
+  app.get('/search-bars', optionalAuth, APIOperations.searchBars); // search for bars, with optional authentication
+  app.get('/bars-user-data', requireAuth, APIOperations.barsUserData); // get user data for the bars that have already been searched for
 }
 
